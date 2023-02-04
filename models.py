32c0386190a238ce0dec5c513352f111e3b1b704
Grisha <dinozzzzzawrik@gmail.com>
2023-02-04 16:41:14 +0200

    setup logic

---
+++ models.py
@@ -0,0 +1,23 @@
+from peewee import *
+
+db = SqliteDatabase('database.db')
+
+
+class WhiteList(Model):
+    id = IntegerField(primary_key=True)
+    name = CharField(null=False)
+    tg_id = CharField(null=False, unique=True)
+
+    class Meta:
+        db_table = 'WhiteList'
+        database = db
+
+
+class Accounts(Model):
+    id = IntegerField(primary_key=True)
+    name = CharField(unique=True)
+    key = CharField(unique=True)
+
+    class Meta:
+        db_table = 'Accounts'
+        database = db
