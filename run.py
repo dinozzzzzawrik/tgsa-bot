bd53810471a383cc4277d19a734a81cf9c7e59d4
Grisha <dinozzzzzawrik@gmail.com>
2023-02-04 15:57:51 +0200

    new project tree

--- run.py
+++ run.py
@@ -1,37 +1,30 @@
-import logging
 import os
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.utils.executor import start_polling
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.handler import Handler
-from aiogram.types import ParseMode
-from aiogram.utils import executor
 
-from bot import handlers
+load_dotenv()
+bot = Bot(token=os.getenv('BOT_TOKEN'))
+dp = Dispatcher(bot)
 
 
-async def on_startup(dp):
-    handlers.setup(dp)
+@dp.message_handler(commands=['start'])
+async def process_start_command(msg: types.Message):
+    await bot.send_message(msg.from_user.id, 'Bot to get Steam Guard Code with telegram')
 
 
-async def on_shutdown(dp):
-    logging.warning("Shutting down..")
-    await dp.bot.session.close()
-    await dp.storage.close()
-    await dp.storage.wait_closed()
-    logging.warning("Bye!")
+@dp.message_handler(commands=['help'])
+async def process_help_command(msg: types.Message):
+    await msg.reply('Admin-panel: url')
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    API_TOKEN = os.getenv('BOT_TOKEN')
-
-    # Configure logging
-    bot = Bot(API_TOKEN)
-    storage = MemoryStorage()
-    dp = Dispatcher(bot, storage=storage)
-
-    start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    executor.start_polling(dp)
